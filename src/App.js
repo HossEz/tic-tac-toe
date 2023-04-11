3014249c64e5889274ce3ee770cf8261264035aa
HossEz <41141267+HossEz@users.noreply.github.com>
2023-04-11 14:29:51 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Det er X sin tur</h3>
+      <div className='board'>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+      </div>
+    </div>
+  );
+}
+export default App;
