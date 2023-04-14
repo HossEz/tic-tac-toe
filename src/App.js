504c5bb735e3d13f0eba84d49937c2bd61dfcc01
HossEz <41141267+HossEz@users.noreply.github.com>
2023-04-14 08:12:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
+import { useState } from 'react';
 import './App.css';
+
+function Square({value, handleInput}){
+  // const [value, setValue] = useState()
+  const handleClick = ()=>{
+    // setValue("X")
+    console.log(value)
+  }
+  return <button className='square' value={value} onClick={handleInput}>{value}</button>
+}
+
+function Board(){
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  const handleClick = (i)=>{
+    const nextSquare = squares.slice()
+    nextSquare[i] = "X"
+    setSquares(nextSquare)
+  }
+
+  return (    
+    <div className='board'>
+      {squares.map((square, i) => (
+        <Square key={i} value={square} handleInput={()=>handleClick(i)} />
+      ))}
+    </div>
+    )
+}
 
 function App() {
   return (
     <div className="App">
       <h3>Det er X sin tur</h3>
-      <div className='board'>
-        <button className='square'>X</button>
-        <button className='square'>X</button>
-        <button className='square'>X</button>
-        <button className='square'>X</button>
-        <button className='square'>X</button>
-        <button className='square'>X</button>
-        <button className='square'>X</button>
-        <button className='square'>X</button>
-        <button className='square'>X</button>
-      </div>
+      <Board />
     </div>
   );
 }
